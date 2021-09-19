83a8d1adfa3052eb6da6538ee910ce44c78980e3
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-19 13:52:23 +0700

    Update theme.dart

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -3,3 +3,15 @@ import 'package:google_fonts/google_fonts.dart';
 
 Color blackColor = Color(0xff3E3939);
 Color creamColor = Color(0xffFFB259);
+Color redColor = Color(0xffFF7676);
+Color greenColor = Color(0xff4CD97B);
+Color purpleColor = Color(0xff9059FF);
+Color mainColor = Color(0xff57BEF7);
+Color whiteColor = Color(0xffFFFFFF);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
