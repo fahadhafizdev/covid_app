896c8bc99fdba25d1229bf1f897e401b1534042c
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-19 14:28:10 +0700

    Create main_page.dart

---
+++ lib/ui/page/main_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:covid_app/shared/theme.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
