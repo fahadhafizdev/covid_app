cb66c9d54530b1e012c7e99fd445075067f5045c
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-20 19:08:24 +0700

    add cubit and state

--- lib/cubit/covid_cubit.dart
+++ lib/cubit/covid_cubit.dart
@@ -1,8 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:covid_app/models/covid_model.dart';
 import 'package:equatable/equatable.dart';
+import 'package:covid_app/service/covid_service.dart';
 
 part 'covid_state.dart';
 
 class CovidCubit extends Cubit<CovidState> {
   CovidCubit() : super(CovidInitial());
+
+  void fetchCovidData() async {
+    try {
+      emit(CovidLoading());
+      List<CovidModel> covidData = await CovidService().fetchCovidData();
+      emit(CovidSuccess(covidData));
+    } catch (e) {
+      emit(CovidFailed(e.toString()));
+    }
+  }
 }
--- lib/cubit/covid_state.dart
+++ lib/cubit/covid_state.dart
@@ -8,3 +8,24 @@ abstract class CovidState extends Equatable {
 }
 
 class CovidInitial extends CovidState {}
+
+class CovidLoading extends CovidState {}
+
+class CovidFailed extends CovidState {
+  final String error;
+
+  CovidFailed(this.error);
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [error];
+}
+
+class CovidSuccess extends CovidState {
+  List<CovidModel> covidData;
+  CovidSuccess(this.covidData);
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [covidData];
+}
