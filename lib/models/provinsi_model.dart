0088da4b0cb9ab266f82afffe0f5c2c59f4aea48
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-24 15:45:31 +0700

    Create provinsi_model.dart

---
+++ lib/models/provinsi_model.dart
@@ -0,0 +1,17 @@
+class ProvinsiModel {
+  final int fid;
+  final int kodeProvinsi;
+  final String provinsi;
+  final int positif;
+  final int sembuh;
+  final int meninggal;
+
+  ProvinsiModel({
+    this.provinsi,
+    this.meninggal,
+    this.positif,
+    this.sembuh,
+    this.fid,
+    this.kodeProvinsi,
+  });
+}
