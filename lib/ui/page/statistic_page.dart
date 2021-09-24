ddee4db68bdc63ebf88eaa8b5a1fec2c28cdd026
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-24 14:26:24 +0700

    update statistic_page.dart and add photos

--- lib/ui/page/statistic_page.dart
+++ lib/ui/page/statistic_page.dart
@@ -1,12 +1,53 @@
+import 'package:covid_app/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 class StatisticPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Statistic page',
-      ),
+    return Stack(
+      children: [
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: mainColor,
+        ),
+        Container(
+          margin: EdgeInsets.only(
+            left: defaultMargin,
+            right: defaultMargin,
+            top: 61,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    'assets/images/icon_menu.png',
+                    height: 24,
+                    width: 18,
+                    fit: BoxFit.fill,
+                  ),
+                  Image.asset(
+                    'assets/images/icon_bell.png',
+                    height: 24,
+                    width: 24,
+                  ),
+                ],
+              ),
+              SizedBox(height: 35),
+              Text(
+                'Data Provinsi',
+                style: whiteTextStyle.copyWith(
+                  fontWeight: semiBold,
+                  fontSize: 24,
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
