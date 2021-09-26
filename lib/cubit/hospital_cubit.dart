a573a3cb98c7539a0c1fe5f4a3c8edb0a0969597
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-26 20:29:07 +0700

    Update hospital_cubit.dart

--- lib/cubit/hospital_cubit.dart
+++ lib/cubit/hospital_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:covid_app/models/hospital_model.dart';
+import 'package:covid_app/service/covid_service.dart';
 import 'package:equatable/equatable.dart';
 
 part 'hospital_state.dart';
 
 class HospitalCubit extends Cubit<HospitalState> {
   HospitalCubit() : super(HospitalInitial());
+
+  void fetchDataHospital() async {
+    try {
+      emit(HospitalLoading());
+      List<HospitalModel> dataHospital =
+          await CovidService().fetchDataHospital();
+      emit(HospitalSuccess(dataHospital));
+    } catch (e) {
+      emit(HospitalFailed(e.toString()));
+    }
+  }
 }
