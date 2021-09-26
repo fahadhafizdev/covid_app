946c4a31e8bc337e68d4252cb033dc30bfebda20
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-26 19:09:32 +0700

    Update hospital_model.dart

--- lib/models/hospital_model.dart
+++ lib/models/hospital_model.dart
@@ -1,7 +1,35 @@
 import 'package:equatable/equatable.dart';
 
 class HospitalModel extends Equatable {
+  final String name;
+  final String address;
+  final String region;
+  final String phone;
+  final String province;
+
+  HospitalModel({
+    this.name,
+    this.address,
+    this.phone,
+    this.province,
+    this.region,
+  });
+
+  factory HospitalModel.fromHson(json) => HospitalModel(
+        name: json['name'],
+        address: json['address'],
+        phone: json['phone'],
+        province: json['province'],
+        region: json['region'],
+      );
+
   @override
   // TODO: implement props
-  List<Object> get props => throw UnimplementedError();
+  List<Object> get props => [
+        name,
+        address,
+        phone,
+        province,
+        region,
+      ];
 }
