0703d0e4894949e8a99ab782be234562a6f06863
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-26 19:03:43 +0700

    create model hospitals

---
+++ lib/models/hospital_model.dart
@@ -0,0 +1,7 @@
+import 'package:equatable/equatable.dart';
+
+class HospitalModel extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object> get props => throw UnimplementedError();
+}
